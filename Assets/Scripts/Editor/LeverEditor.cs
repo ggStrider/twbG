501b7d23263422e488b1af63bdccdb5eb31a0c4d
Yaroslav Oslam <ggStrider1@gmail.com>
2024-11-03 11:22:38 +0200

    Added Reset Lever Type button to lever component

---
+++ Assets/Scripts/Editor/LeverEditor.cs
@@ -0,0 +1,23 @@
+﻿using Interact.Actions;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(InteractLever))]
+    public class InteractLeverEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        
+            GUILayout.Space(10);
+            
+            var lever = (InteractLever)target;
+            if (GUILayout.Button("Reset Lever Type"))
+            {
+                lever._ResetLeverType();
+            }
+        }
+    }
+}
